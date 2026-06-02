3e4a0e628e7f77b131849a3f0948a98f8e1fc0fa
Volkan Genç <gencvolkan@gmail.com>
2026-06-03 00:03:57 +0300

    confusion_matrix added

--- naivebayes.py
+++ naivebayes.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from mixed_naive_bayes import MixedNB
@@ -69,3 +70,29 @@ print(kagetorikNitelikler)
 nb_model = MixedNB(categorical_features=kagetorikNitelikler)
 nb_model.fit(X_train,y_train)
 
+# Performans Değerlendirme
+# Naive Bayes sınıflandırıcı modelinin performansının değerlendirilebilmesi için nb_model.predict() fonksiyonundan faydalanılmıştır.
+# Modelin tahminleri ve test veri setinin hedef niteliği görüntülenirse
+# LabelEncoder() ile sayısal hale dönüştürüldüğü biçimiyle yer almakta oldukları görülebilir.
+# İlgili değerleri kategorik hale (orijinal haline) döndürmek için 
+# label_encoder.inverse_transform() kullanılmıştır.
+# Bu adım elde edilecek olan kontenjans tablosunun daha iyi yorumlanabilmesini sağlamak amacıyla yapılmıştır.
+
+y_tahmin = nb_model.predict(X_test)
+y_tahmin = label_encoder.inverse_transform(y_tahmin)
+y_test = label_encoder.inverse_transform(y_test)
+
+print("--- ytahmin----")
+print(y_tahmin)
+print("--- ytest ----")
+print(y_test)
+
+# Kontenjans tablosu sklearn.metrics içindeki confusion_matrix() ile elde edilmiştir.
+# “yes”, yani evet sınıfı pozitif sınıf olarak kabul edilmiştir. 
+# Ardından, kontenjans tablosunun görsel açıdan daha iyi biçimde oluşturulabilmesi için ConfusionMatrixDisplay() fonksiyonu kullanılmıştır.
+
+my_cm = confusion_matrix(y_true=y_test,y_pred=y_tahmin,labels=["no","yes"])
+my_cm_p = ConfusionMatrixDisplay(my_cm,display_labels=["no","yes"])
+
+my_cm_p.plot()
+plt.show()
